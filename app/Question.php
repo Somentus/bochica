edc99aff8523146b74e83c53983d611303ae7b9d
Somentus <somentus@gmail.com>
2018-06-14 01:12:02 +0200

    Add relationship between User and Question

--- app/Question.php
+++ app/Question.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     protected $fillable = ['user_id', 'title', 'body'];
+
+    public function user()
+    {
+    	return $this->belongsTo('App\User');
+    }
 }
