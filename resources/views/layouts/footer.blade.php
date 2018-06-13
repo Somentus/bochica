b7db563b283a5978d11c7182a6968c2b6e115742
Somentus <somentus@gmail.com>
2018-06-14 01:44:36 +0200

    Add footer.

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,23 @@
+<hr>
+<div class="row justify-content-center">
+	<div class="col-md-1">
+		<a href="/faq">
+			FAQ
+		</a>
+	</div>
+	<div class="col-md-1">
+		<a href="/contact">
+			Contact
+		</a>
+	</div>
+	<div class="col-md-1">
+		<a href="/legal">
+			<i class="far fa-copyright"></i> 2018
+		</a>
+	</div>
+	<div class="col-md-1">
+		<a href="/tutorials">
+			Tutorials
+		</a>
+	</div>
+</div>
