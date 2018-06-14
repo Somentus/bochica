ea0309259237d7bd4a614bf7c05210df138a5a6e
Somentus <somentus@gmail.com>
2018-06-14 16:44:21 +0200

    Rework questions.show

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -1,6 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-	<h5>{{ $question->title }}</h5>
-	{{ $question->body }}
+	<div class="container">
+        <div class="row pb-3">
+            <div class="border p-3 w-100" >
+                <div class="row">
+                    <div class="col-md-9">    
+                        <h5><strong>
+                            {{ $question->title }}
+                        </h5></strong>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="float-right">
+                            X Points
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <small>
+                            {{ $question->user->name }}, 
+                            {{ \Carbon\Carbon::createFromTimeStamp(strtotime($question->created_at))->diffForHumans() }}
+                        </small>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <p>
+                            {{ $question->body }}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+	</div>
 @endsection
